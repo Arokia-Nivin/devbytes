344437d068bd0240d4eb7f498906e4d275ebbf23
Arokia-Nivin <nivinjoseph.a@gmail.com>
2021-10-29 00:38:12 +0530

    refactored route handlers

--- server/index.js
+++ server/index.js
@@ -4,54 +4,35 @@ const keys=require("./keys");
 
 const mongoose=require("mongoose"); 
 mongoose.connect(keys.MONGOURI); 
-const Event=require("./models/events"); 
+
 
 app.use(express.urlencoded({extended:true})); 
 app.get("/",(req,res)=>{
-    res.send("<h1>devbyts</h1>");
+    res.send("<h1>website under development</h1>");
 })
 
-app.get("/api/events/dates", async(req,res)=>{
-    try{
-        const events= await Event.find({}, {"eventdate":1});
-        res.send(events); //dates will be sent 
-    }
-    catch(err) {
-        res.status(500).send(err);
-    }
-})
+//eventRoute Handlers
+const eventRoutes=require("./routes/eventRoutes"); 
+app.use("/api/events",  eventRoutes);
+
+//projectRoute Handlers 
+const projectRoutes=require("./routes/projectRoutes"); 
+app.use("/api/projects",projectRoutes);
+//adminRoute Handlers 
+const adminRoutes=require("./routes/adminRoutes"); 
+app.use("/api/admin", adminRoutes); 
 
 
-app.get("/api/events/:date", async (req,res)=>{
-    const date= new Date(req.params.date);
-    const isValidDate= (date !== "Invalid Date" && !isNaN(date));
-    if (isValidDate)
-    {
-        try{
-            const events = await  Event.find({eventdate:date});
-            res.send({events});
-        }
-        catch(err)
-        {
-            res.status(500).send(err);
-        }
 
-    }
-    else 
-    {
-        //incorrect request body 
-        res.status(400).send("Invalid Date");
-    }
-    
-  
-
-}) 
-
-app.get("/admin",(req,res)=>{
-    res.send("I am admin");
-})
 const PORT=5000;
 app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
+    if (process.env.ENVIRONMENT!=="production")
+    {
+
+        console.log(`listening on port http://localhost:${PORT}/`);
+        // require('child_process').exec('start http://localhost:5000/');
+
+    }
+    
 });
 
