bba58ece551829c82c335b6bfbf87cee4c37e2ba
Altair-1165 <s121aravind@gmail.com>
2021-10-28 20:17:39 +0530

    react-router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Calendar from './components/calendarComponet/Calendar';
 import NavbarComponent from './components/NavbarComponent';
+import ProjectComponent from './components/ProjectComponent';
 
 
 class App extends React.Component {
   render(){
-    
     return (
       <React.Fragment>
         <NavbarComponent/>
         <div className="container-md" style={{marginTop: '70px'}}>
-          <Calendar />
+          <Route exact path="/events" component={Calendar}/>
+          <Route exact path="/projects" component={ProjectComponent}/>
         </div>
       </React.Fragment>
     );
--- client/src/components/NavbarComponent.js
+++ client/src/components/NavbarComponent.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import {Navbar,Nav,Container} from "react-bootstrap";
 
 function NavbarComponent() {
@@ -6,13 +7,13 @@ function NavbarComponent() {
         <div className="NavbarComponent">
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
               <Container>
-              <Navbar.Brand href="#home">Code-club</Navbar.Brand>
+              <Navbar.Brand><NavLink style={{ textDecoration: "none", color: "white" }} to="/">Code-club</NavLink></Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="navbar-links">
-                  <Nav.Link  href="#events">Events</Nav.Link>
-                  <Nav.Link  href="#projects">Projects</Nav.Link>
-                  <Nav.Link href="#Contact Us">Contact Us</Nav.Link>
+                  <Nav.Link><NavLink style={{ textDecoration: "none", color: "white" }} to="/events">Events</NavLink></Nav.Link>
+                  <Nav.Link><NavLink style={{ textDecoration: "none", color: "white" }} to="/projects">Projects</NavLink></Nav.Link>
+                  <Nav.Link><NavLink style={{ textDecoration: "none", color: "white" }} to="/contact">Contact</NavLink></Nav.Link>
                 </Nav>
               </Navbar.Collapse>
               </Container>
