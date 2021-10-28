c49735eeb7e743efadde5d2007598b2b9fd11b3e
SIvaram0108 <65078129+SIvaram0108@users.noreply.github.com>
2021-10-28 14:50:37 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Navbar from './components/NavbarComponent';
 import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+
 
 function App() {
   return (
     <div className="App">
-
+      <NavbarComponent/>
     </div>
   );
 }
