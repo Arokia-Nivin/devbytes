2464a32ba8623fc71ff6c7097b208d94e5ef1524
Arokia-Nivin <nivinjoseph.a@gmail.com>
2021-10-29 00:56:35 +0530

    updated ProjectRoutes.js file

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,9 +1,19 @@
 const express=require("express"); 
 const router=express.Router(); 
-const Event=require("../models/projects"); 
+const Project=require("../models/projects"); 
 
-router.get("/",(req,res)=>{
-    res.send("projects list will be sent from here");
+router.get("/", async (req,res)=>{
+    try
+    {
+        const  projects=await Project.find({});
+        res.send({projects}); 
+
+    }
+    catch(err)
+    {
+        res.status(500).send(err); 
+    }
+
 })
 
 module.exports=router; 
