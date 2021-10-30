0f9a34ed962d65dbebdf25d29034bb388b2cdd93
Arokia-Nivin <nivinjoseph.a@gmail.com>
2021-10-30 21:07:55 +0530

    made some minor changes before deploy

--- server/client/src/components/ProjectComponent.js
+++ server/client/src/components/ProjectComponent.js
@@ -43,8 +43,6 @@ export class ProjectComponent extends Component {
                             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                             </svg>
                             <p className="card-text">{item.description}</p>
-                            <p className="card-text">{item.description}</p>
-
                         </div>
 
                     </div>
@@ -82,7 +80,7 @@ export class ProjectComponent extends Component {
     render() {
         return (
             <div>
-                <h1>Our projects</h1>
+                <h1 className="text-center" >Our projects</h1>
                 {this.renderProjects()}
             </div>
         )
