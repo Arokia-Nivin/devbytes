457fbe7e976305e1708a7a4dfafee10a178d7c13
Arokia-Nivin <nivinjoseph.a@gmail.com>
2021-11-01 05:50:59 +0530

    added scrollToTop

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ScrollToTop  from './ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ScrollToTop>
+      <App/>
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById('root')
 );
