97190fbce02eac172590dc036905c6b14e590d19
Arokia-Nivin <nivinjoseph.a@gmail.com>
2021-11-01 08:21:15 +0530

    final deploy

--- server/client/src/components/admindashboard.jsx
+++ server/client/src/components/admindashboard.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 const Dashboard = () => {
     return (
-        
         <div style={{ marginTop: "45vh", display:"flex", justifyContent: "center", alignItems: "center"}}  >
             <div style={{fontSize:"50px"}}>👋 Hello Admin</div>
         </div>
-       
     )
 }
 
